0cace7ecb437b0e36d1fb679d6898650321bd07f
Hoon <ldh019@naver.com>
2024-04-25 21:27:48 +0900

    Update RecordUiState : Add component

--- app/src/main/java/com/gdsc_cau/vridge/ui/record/RecordUiState.kt
+++ app/src/main/java/com/gdsc_cau/vridge/ui/record/RecordUiState.kt
@@ -1,4 +1,19 @@
 package com.gdsc_cau.vridge.ui.record
 
-interface RecordUiState {
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.Stable
+
+@Stable
+sealed interface RecordUiState {
+    @Immutable
+    data object Loading : RecordUiState
+
+    @Immutable
+    data class Success(
+        val index: Int,
+        val text: String,
+        val isRecording: Boolean,
+        val isFinished: Boolean,
+        val isLoading: Boolean
+    ) : RecordUiState
 }
