64ddca99fe54112c36869c72f6913180e2e43753
hoon <ldh019@naver.com>
2024-04-30 17:12:26 +0900

    Update LoginDTO : Add Serializable annotation

--- app/src/main/java/com/gdsc_cau/vridge/data/dto/LoginDTO.kt
+++ app/src/main/java/com/gdsc_cau/vridge/data/dto/LoginDTO.kt
@@ -1,7 +1,9 @@
 package com.gdsc_cau.vridge.data.dto
 
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class LoginDTO(
     @SerialName("token") val token: String,
 )
