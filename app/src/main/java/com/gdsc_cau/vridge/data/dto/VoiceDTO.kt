c66c50a2eb90696c026fb6ced0af1fe22b1100fc
hoon <ldh019@naver.com>
2024-04-30 17:13:00 +0900

    Update VoiceDTO : Add Serializable annotation

--- app/src/main/java/com/gdsc_cau/vridge/data/dto/VoiceDTO.kt
+++ app/src/main/java/com/gdsc_cau/vridge/data/dto/VoiceDTO.kt
@@ -1,7 +1,9 @@
 package com.gdsc_cau.vridge.data.dto
 
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class VoiceDTO(
     @SerialName("uid") val uid: String,
     @SerialName("vid") val vid: String,
