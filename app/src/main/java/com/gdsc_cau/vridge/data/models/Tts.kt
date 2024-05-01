90567f3f8cb4fb5ca93c57d787f6c77db4a3347d
hoon <ldh019@naver.com>
2024-05-01 21:45:21 +0900

    Update Tts : Add Serializable annotation

--- app/src/main/java/com/gdsc_cau/vridge/data/models/Tts.kt
+++ app/src/main/java/com/gdsc_cau/vridge/data/models/Tts.kt
@@ -1,8 +1,11 @@
 package com.gdsc_cau.vridge.data.models
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Tts(
     val id: String,
     val text: String,
     val timestamp: Long,
-    val state: Boolean = false
+    val status: Boolean = false
 )
