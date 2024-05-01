ece6c534be899887e5a7d12381fea9d86e7858ff
hoon <ldh019@naver.com>
2024-05-01 21:45:47 +0900

    Update User : Add Serializable annotation

--- app/src/main/java/com/gdsc_cau/vridge/data/models/User.kt
+++ app/src/main/java/com/gdsc_cau/vridge/data/models/User.kt
@@ -1,9 +1,8 @@
 package com.gdsc_cau.vridge.data.models
 
 data class User(
-    val cntVoice: Int = -1,
+    val uid: String = "",
+    val cntVoice: Int = 0,
     val email: String = "",
-    val gender: Gender = Gender.HIDDEN,
     val name: String = "",
-    val uid: String = ""
 )
