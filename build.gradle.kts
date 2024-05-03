c17ff4b14223f5d71179bec97e5aa6df979f22cf
hoon <ldh019@naver.com>
2024-05-03 20:28:04 +0900

    Update module build.gradle : Add fcm library

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,11 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.51.1")
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.4.10")
+        classpath("com.google.gms:google-services:4.4.1")
     }
 }
 
 plugins {
-    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.google.gms.google-services") version "4.4.1" apply false
     id("com.google.firebase.crashlytics") version "2.9.9" apply false
 }
